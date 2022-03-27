92a3ec1e1ccfc35de35ba6307e1f9727c1b33710
Bhavana Srivastava <swtbhanu8@gmail.com>
2022-03-27 22:11:56 +0530

    Assignment 10 spring boot

---
+++ Assignment-10 Spring MS/boot_ms/coupon/src/main/java/com/coupon/CouponApplication.java
@@ -0,0 +1,32 @@
+package com.coupon;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import com.coupon.entities.Coupon;
+import com.coupon.service.CouponService;
+@EnableEurekaClient
+@SpringBootApplication
+public class CouponApplication implements CommandLineRunner{
+
+	@Autowired
+	private CouponService couponService;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CouponApplication.class, args);
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Added----");
+		couponService.addCoupon(new Coupon("SUP20", 20, new Date()));
+		couponService.addCoupon(new Coupon("SUP10", 10, new Date()));
+	}
+
+}
---
+++ Assignment-10 Springboot/boot_ms/product/src/main/java/com/product/ProductApplication.java
@@ -0,0 +1,29 @@
+package com.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import com.product.entity.Product;
+import com.product.service.ProductService;
+@EnableEurekaClient
+@SpringBootApplication
+public class ProductApplication implements CommandLineRunner{
+
+	@Autowired
+	private ProductService productService;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		productService.addProduct(new Product("HP laptop", 60000));
+		productService.addProduct(new Product("LG TV", 70000));
+		
+	}
+
+}
